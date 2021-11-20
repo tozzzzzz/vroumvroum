515e7b0d0e32dbc71cf497bdf69e8b9c89d6f91d
C.BOIS <62143578+tozzzzzz@users.noreply.github.com>
2021-11-20 14:30:33 +0100

    v1

---
+++ ProjetTuturElecBois.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 30 10:51:05 2021
+
+@author: Boisced
+"""
+
+import requests
+
+
+from zeep import Client
+
+
+
+
+class App():
+	def __init__(self):
+		pass
+	
+	
+	
+	def BorneElec(self,X,Y,R,N):
+		
+		
+		listeBorne=[]
+		T=0
+		
+		
+		URL = "https://opendata.reseaux-energies.fr/api/records/1.0/search/?dataset=bornes-irve&q=&rows="+str(N)+"&geofilter.distance="+str(X)+"%2C"+str(Y)+"%2C"+str(R)
+
+
+		r = requests.get(url = URL)
+		
+		data = r.json()
+		
+		for i in data['records']:
+			bornes=[]
+			
+			bornes.append(T)
+			bornes.append(i['recordid'])
+			bornes.append(i['fields']['ylatitude'])
+			bornes.append(i['fields']['xlongitude'])
+			bornes.append(i['fields']['acces_recharge'])
+			listeBorne.append(bornes)
+			T=T+1
+			
+		return listeBorne
+
+	def calculeTrajet(self,ville1,ville2):
+		None
+		
+		
+	def trouverCoordonéeVille(self,ville):
+		adresse=[]
+	
+		
+		
+		URL = "https://nominatim.openstreetmap.org/search?q="+str(ville)+"&format=json&polygon_geojson=1&addressdetails=1"
+
+
+		r = requests.get(url = URL)
+		
+		donnee = r.json()
+		adresse.append(donnee[0]['lat'])
+		adresse.append(donnee[0]['lon'])
+			
+		return adresse
+	
+	
+	def get_distance2(self,Xville1,Yville1,Xville2,Yville2):
+	
+		wsdl = 'http://127.0.0.1:8000/?wsdl'
+		client = Client(wsdl=wsdl)
+
+		return client.service.get_distance(Xville1,Yville1,Xville2,Yville2)
+		
+	def get_time(self,distance, vitesse):
+		return float(distance) / vitesse
+	
+    
+   
+    
+        
+		
+
+app1 =App()
+
+x=48.8520930694
+y=2.34738897685
+r=1000
+n=1
+
+print(app1.BorneElec(x,y,r,n))
+
+Ville1=app1.trouverCoordonéeVille("Marignier")
+Ville2=app1.trouverCoordonéeVille("Paris")
+print(Ville1)
+print(Ville2)
+
+print("avant")
+a=app1.get_distance2(Ville1[0], Ville1[1], Ville2[0], Ville2[1])
+print("la distance est "+a)
+print(a)
+print("la vitesse est "+app1.get_time(a,130))
+print("apres")
+
+
+
+
---
+++ serveurSoapTuturElecBois.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  7 11:05:47 2021
+
+@author: user
+"""
+
+
+
+import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+from spyne import Application, rpc, ServiceBase, Integer, Unicode, AnyDict, Double
+from spyne import Iterable
+from spyne.protocol.soap import Soap11
+from spyne.protocol.xml import XmlDocument
+from spyne.server.wsgi import WsgiApplication
+
+
+
+
+
+from math import acos, asin, atan, atan2, ceil, cos, cosh, degrees
+from math import exp, fabs, floor, fmod, frexp, hypot, ldexp, log
+from math import log10, modf, pi, pow, radians, sin, sinh, sqrt
+from math import tan, tanh
+
+
+class CalculeDistance(ServiceBase):
+	@rpc(Unicode, Unicode, Unicode, Unicode, _returns=Integer)
+	def get_distance(ctx, lat_1, lng_1, lat_2, lng_2): 
+		   lng_1, lat_1, lng_2, lat_2 = map(radians, [float(lng_1), float(lat_1), float(lng_2), float(lat_2)])
+		   d_lat = lat_2 - lat_1
+		   d_lng = lng_2 - lng_1 
+
+		   temp = (  
+			      sin(d_lat / 2) ** 2 
+				  + cos(lat_1) 
+				  * cos(lat_2) 
+				  * sin(d_lng / 2) ** 2
+		   )
+		   a=6373 * (2 * atan2(sqrt(temp), sqrt(1 - temp)))
+
+           
+		   return a
+	   
+	        
+
+
+			
+application = Application([CalculeDistance],
+    tns='spyne.examples.hello',
+    in_protocol=Soap11(validator='lxml'),
+    out_protocol=Soap11()
+)
+
+
+
+if __name__ == '__main__':
+    # You can use any Wsgi server. Here, we chose
+    # Python's built-in wsgi server but you're not
+    # supposed to use it in production.
+    from wsgiref.simple_server import make_server
+    wsgi_app = WsgiApplication(application)
+    server = make_server('127.0.0.1', 8000, wsgi_app)
+    server.serve_forever()
